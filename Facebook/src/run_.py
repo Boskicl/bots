124e512a1b1da76ab8ec54fe564b767517f0414d
Ljube_Windows <Ljuboslavboskic@gmail.com>
2020-05-17 09:53:59 -0700

    Added feature to get to birthdays tab. Need a way to see number of birthdays.

--- Facebook/src/run_.py
+++ Facebook/src/run_.py
@@ -26,9 +26,10 @@ class FaceBot:
     def Birthdays(self):
         sleep(1)
         driver = self.driver
-        driver.find_element_by_class_name('gs1a9yip ow4ym5g4 auili1gw rq0escxv j83agx80 cbu4d94t buofh1pr g5gj957u i1fnvgqd oygrvhab cxmmr5t8 hcukyx3x kvgmc6g5 tgvbjcpo hpfvmrgz rz4wbd8a a8nywdso l9j0dhe7 du4w35lb rj1gh0hx f10w8fjw pybr56ya').click()
+        driver.find_element_by_css_selector('#mount_0_0 > div > div > div.rq0escxv.l9j0dhe7.du4w35lb > div > div > div.j83agx80.cbu4d94t.d6urw2fd.dp1hu0rb.l9j0dhe7.du4w35lb > div > div.rq0escxv.l9j0dhe7.tkr6xdv7.j83agx80.cbu4d94t.d2edcug0.pfnyh3mw.dp1hu0rb.rek2kq2y.o36gj0jk > div > div.q5bimw55.ofs802cu.dkue75c7.mb9wzai9.o8kakjsu.rpm2j7zs.k7i0oixp.gvuykj2m.j83agx80.cbu4d94t.ni8dbmo4.eg9m0zos.buofh1pr.l56l04vs.r57mb794.l9j0dhe7.kh7kg01d.c3g1iek1.k4xni2cv > div.a8s20v7p.k5wvi7nf.buofh1pr.pfnyh3mw.l9j0dhe7.du4w35lb > div.aov4n071 > div:nth-child(3) > a > div.ow4ym5g4.auili1gw.rq0escxv.j83agx80.buofh1pr.g5gj957u.i1fnvgqd.oygrvhab.cxmmr5t8.hcukyx3x.kvgmc6g5.nnctdnn4.hpfvmrgz.qt6c0cv9.jb3vyjys.l9j0dhe7.du4w35lb.bp9cbjyn.btwxx1t3.dflh9lhu.scb9dxdr').click()
+        # Working on getting number of birthdays present.... can select birthdays for now
+        print(driver.find_elements_by_css_selector('#mount_0_0 > div > div > div.rq0escxv.l9j0dhe7.du4w35lb > div > div > div.j83agx80.cbu4d94t.d6urw2fd.dp1hu0rb.l9j0dhe7.du4w35lb > div > div.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.cbu4d94t.d2edcug0.rj1gh0hx.buofh1pr.g5gj957u.hpfvmrgz.dp1hu0rb > div > div > div > div > div:nth-child(1) > div > div > div > div.dati1w0a.qt6c0cv9.hv4rvrfc.jb3vyjys.b20td4e0'))
 
-      
 
     def findfriend(self,person):
 #            """
@@ -120,10 +121,10 @@ class FaceBot:
  
 if __name__ == '__main__':
 
-    usr = str(input('What is your Facebook username? (case sensitive): '))
-    paswrd = getpass.getpass(prompt='What is your Facebook password (case sensitive): ', stream=None)
-
+    #usr = str(input('What is your Facebook username? (case sensitive): '))
+    #paswrd = getpass.getpass(prompt='What is your Facebook password (case sensitive): ', stream=None)
     face = FaceBot(usr,paswrd)
     face.login() 
     face.Events()
+    face.Birthdays()
 
